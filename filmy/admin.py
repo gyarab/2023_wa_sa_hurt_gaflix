f7e231d484c9c15023a26966f5681715f3a27be1
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2024-05-24 11:10:19 +0200

    Updated Movies admin

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from filmy.models import Movie, Director, Genre
+from filmy.models import Movie, Director, Genre, Actor
 
 
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "year", "footage", "director", "genres_display"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name", "director__name"]
+    list_filter = ["year", "genres"]
+    list_editable = ["year", "footage"]
 
+class ActorAdmin(admin.ModelAdmin):
+    pass
 
 class DirectorAdmin(admin.ModelAdmin):
     pass
@@ -15,6 +21,8 @@ class GenreAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Movie, MovieAdmin)
+
+admin.site.register(Actor, ActorAdmin)
 
 admin.site.register(Director, DirectorAdmin)
 
