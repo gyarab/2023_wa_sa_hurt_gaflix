ef45d5209ad53423269195296125dcd26c16c70e
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2024-05-31 11:33:06 +0200

    added search and genre filter

--- filmy/views.py
+++ filmy/views.py
@@ -1,10 +1,22 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-from filmy.models import Movie
+from filmy.models import Movie, Genre
 
 def movies(request):
+    movies_queryset = Movie.objects.all().order_by('name')
+    genre = request.GET.get('genre')
+    if genre:
+        movies_queryset = movies_queryset.filter(genres__name=genre)
+    search = request.GET.get('search')
+    if search:
+        movies_queryset = movies_queryset.filter(Q(name__icontains=search)|Q(description__icontains=search)) 
+
     context = {
-        'movies': Movie.objects.all().order_by('name')
+        "movies": movies_queryset,
+        "genres": Genre.objects.all().order_by('name'),
+        "genre": genre,
+        "search": search,
     }
     return render(request, 'filmy/movies.html', context)
 
