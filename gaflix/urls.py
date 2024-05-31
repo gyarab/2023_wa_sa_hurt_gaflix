bfcb7b4f46cd1b4d511ef7be6c58abb238b90e44
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2024-05-31 10:09:39 +0200

    created film listing

--- gaflix/urls.py
+++ gaflix/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
+from filmy.views import movies
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  TemplateView.as_view(template_name="filmy/home.html")),
-    path('druhy/',  TemplateView.as_view(template_name="filmy/druhy.html"))
+    path('druhy/',  TemplateView.as_view(template_name="filmy/druhy.html")),
+    path('filmy/', movies, name="movies"),
 ]
